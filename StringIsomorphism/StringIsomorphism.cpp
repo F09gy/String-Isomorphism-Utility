36e89bfcdc93e6dad927ffdbe57a98540088e6b7
F09gy <kalpatov@outlook.com>
2026-06-16 15:22:06 +0300

    Написан код для открытия входного файла и чтения содержимого (readInputFile)

--- StringIsomorphism/StringIsomorphism.cpp
+++ StringIsomorphism/StringIsomorphism.cpp
@@ -1,46 +1,54 @@
-﻿#include <iostream>
+﻿#include "StringIsomorphism.h"
+#include <iostream>
 #include <string>
 #include <map>
 #include <vector>
 #include <fstream>
 
-enum class ErrorCode
-{
-    SUCCESS,
-    ERR_ARGS_COUNT, 
-    ERR_INPUT_FILE_NOT_FOUND, 
-    ERR_OUTPUT_FILE_CREATE, 
-    ERR_INVALID_CHARS, 
-    ERR_LINE_LENGTH, 
-    ERR_READ
-};
-
-// Функция чтения входного файла
+/**
+ * \brief Функция чтения входного файла
+ * \param[in] path - путь к входному файлу
+ * \param[out] lines - ссылка на вектор для сохранения прочитанных строк
+ * \return - true если файл успешно прочитан, иначе false
+ */
 bool readInputFile(std::string path, std::vector<std::string>& lines)
 {
+    std::ifstream file(path);
 
+    if (!file.is_open())
+    {
+        return false;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        lines.push_back(line);
+    }
+
+    return true;
 }
 
-// Функция проверки изоморфности
+/**
+* \brief Проверяет изоморфность двух строк и формирует словарь символьных соответствий
+* \param[in] str1 - первая входная строка
+* \param[in] str2 - вторая входная строка
+* \param[out] mapping - ссылка на std::map для записи правил перевода (char1 -> char2)
+* \return - true, если строки изоморфны; false, если не изоморфны (длины не равны или нарушена биективность)
+*/
 bool solveIsomorphism(std::string str1, std::string str2, std::map<char, char>& mapping)
 {
 
 }
 
-// Функция создания файла и записи результата
+/**
+ * \brief Функция записи результата в выходной файл
+ * \param path[in] - путь к выходному файлу
+ * \param isIsomorphic[in] - флаг изоморфности строк
+ * \param mapping[in, out] - словарь соответствий (может быть пустым)
+ * \return - true если файл успешно записан, иначе false
+ */
 bool writeOutputFile(std::string path, bool isIsomorphic, const std::map<char, char>& mapping)
 {
 
-}
-
-int main(int argc, char* argv[])
-{
-    if (argc != 3)
-    {
-        std::cerr << "Ошибка ввода: аргументов меньше или больше, чем 2." << std::endl;
-        return static_cast<int>(ErrorCode::ERR_ARGS_COUNT);
-    }
-
-    std::string inputPath = argv[1];
-    std::string outputName = argv[2];
 }
